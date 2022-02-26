16bf28c0456e734d5bc0f5479d1a9c1eca5c4ab9
tarcisio <tarcisioemanoel10031@gmail.com>
2022-02-25 23:22:35 -0300

    Refazendo do zero

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
-import store, { persistor } from './store';
-import history from './services/history';
 import GlobalStyle from './styles/GlobalStyles';
-import Header from './components/Header';
-import Routes from './routes';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Router history={history}>
-          <Header />
-          <Routes />
-          <GlobalStyle />
-          <ToastContainer autoClose={3000} className="toast-container" />
-        </Router>
-      </PersistGate>
-    </Provider>
+    <>
+      <Login />
+      <GlobalStyle />
+    </>
   );
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
-import { Title, Paragrafo } from './styled';
-import * as exampleActions from '../../store/modules/example/actions';
+import { Paragrafo } from './styled';
 
 export default function Login() {
-  const dispatch = useDispatch();
-
-  function handleClick(e) {
-    e.preventDefault();
-
-    dispatch(exampleActions.clicaBotaoRequest());
-  }
-
   return (
     <Container>
-      <Title>
+      <h1>
         Login
         <small>Oie</small>
-      </Title>
+      </h1>
       <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
-      <button type="button" onClick={handleClick}>
-        Enviar
-      </button>
+      Enviar
     </Container>
   );
 }
